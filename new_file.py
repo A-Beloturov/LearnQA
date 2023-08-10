575ec18b5fdf6a59f562fd38ac3d44e590cd3eb9
A-Beloturov <a.beloturov@bk.ru>
2023-08-10 15:55:15 +0400

    Typing

--- new_file.py
+++ new_file.py
@@ -1,20 +1,22 @@
 import typing
+import time
+from typing import Union
 
 
 class Car:
-    def __init__(self, model, horsepowers):
-        self.model: str = model
-        self.horsepowers: int = horsepowers
+    def __init__(self, model: str, horsepowers: int):
+        self.model = model
+        self.horsepowers = horsepowers
 
     def __str__(self):
         return f"Model: {self.model}, Horsepowers: {self.horsepowers}"
 
 
-cars_parameters: list = [{"model": "nissan", "horsepowers": 120}, {"model": "honda", "horsepowers": 150}]
+cars_parameters: list[str, int] = [{"model": "nissan", "horsepowers": 120}, {"model": "honda", "horsepowers": 150}]
 
 
-def car_creation(parameters: list) -> list:
-    car_obj: list = []
+def car_creation(parameters: list[dict[str, Union[int, str]]]) -> list[Car]:
+    car_obj = []
     for item in parameters:
         model = item['model']
         horsepowers = item['horsepowers']
