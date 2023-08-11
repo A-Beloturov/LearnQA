0fbbe52bdbcc243778c27a902a3a626bafe8eaf4
A-Beloturov <a.beloturov@bk.ru>
2023-08-11 16:41:22 +0300

    Type

--- new_file.py
+++ new_file.py
@@ -12,7 +12,7 @@ class Car:
         return f"Model: {self.model}, Horsepowers: {self.horsepowers}"
 
 
-cars_parameters: list[str, int] = [{"model": "nissan", "horsepowers": 120}, {"model": "honda", "horsepowers": 150}]
+cars_parameters: list[dict[str, Union[int, str]]] = [{"model": "nissan", "horsepowers": 120}, {"model": "honda", "horsepowers": 150}]
 
 
 def car_creation(parameters: list[dict[str, Union[int, str]]]) -> list[Car]:
@@ -26,3 +26,4 @@ def car_creation(parameters: list[dict[str, Union[int, str]]]) -> list[Car]:
 
 car_1, car_2 = car_creation(cars_parameters)
 print(car_1, car_2)
+
