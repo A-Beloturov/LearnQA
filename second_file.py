bcf0d3f3607745e81dde4ea109c0e78cc23f4b08
A-Beloturov <a.beloturov@bk.ru>
2023-08-13 13:08:41 +0400

    List_function

--- second_file.py
+++ second_file.py
@@ -1,25 +1,39 @@
 def func_a(*args):
-    print(sum(args))
-
-
-def func_b(*args):
     print(f'В меня передали: {args}')
 
 
-def func_c(*args):
+
+def func_b(*args):
     print(f'А в меня передали: {args}')
 
 
-list_func = [func_a(), func_b(), func_c()]
+def func_c(*args):
+    print(f'А я слива лиловая, спелая, садовая, с аргументами: {args}')
 
-x = 6
-y = 6
-z = 6
 
-list_arg = [x, y, z]
+list_func = [func_a, func_b, func_c]
+
+x = "Проверка"
+y = 7
+z = 8
+
+list_arg = [(x, y, z)]
+
+
+# def training_func(first_list, second_list):
+#     for first in first_list:
+#         first_result = []
+#         for second in second_list:
+#             first_result.append(first(*second))
+#     return first_result
+
 
 
 def training_func(first_list, second_list):
-    pass
+    for first in first_list:
+        for second in second_list:
+            first(*second)
 
-# training_func(list_func, list_arg)
+
+
+training_func(list_func, list_arg)
